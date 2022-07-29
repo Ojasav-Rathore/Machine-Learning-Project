0606f2f23e1020efa5e8b246232fcc939812be9c
Coding Budha <109053998+Ojasav-Rathore@users.noreply.github.com>
2022-07-29 17:45:39 +0530

    The api call to predict the costs The result is the predicted cost by the model in string format The input still needs to be in the fitted format which is one thing yet to be resolved

---
+++ Linear_model.py
@@ -0,0 +1,26 @@
+from distutils.log import debug
+import joblib
+from flask import Flask, request
+import json
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+app = Flask(__name__)
+
+model = joblib.load(r'C:\Users\Dell\Documents\GitHub\data-science-project\Linear_model')
+
+@app.route('/predict_cost',methods=['POST'])
+
+def predict_cost():
+    event = json.loads(request.data)
+    values = event['values']
+    values = list(map(np.float, values))
+    pre = np.array(values)
+    pre = pre.reshape(1,-1)
+    #pre = scaler.transform(pre)
+    res = model.predict(pre)
+    print(res)
+    return str(res[0])
+
+if __name__ == '__main__':
+    app.run(debug = True)
